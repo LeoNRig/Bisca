af540956524136f2e2e4a3fd7e7797f2344ac541
filipe <filipe.ogramlich@gmail.com>
2022-12-10 12:10:37 -0300

    Arquivo da partida

---
+++ src/main/java/Partida.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Windows
+ */
+public class Partida {
+    public static void main(String[] args) {
+        System.out.println("BEM VINDO AO JOGO DE BISCA!!!!!!");
+    }
+}
